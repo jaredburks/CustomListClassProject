cfa78abb7c3722e9b817fefd27d0425d23522a4e
jaredburks <jarabu93@gmail.com>
2017-09-20 13:31:42 -0500

    Added an Add, Remove, Clear test method.

--- CustomListClass/CustomListClass/CustomListTest.cs
+++ CustomListClass/CustomListClass/CustomListTest.cs
@@ -7,8 +7,37 @@ namespace CustomListClass
     public class CustomListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddInt()
         {
+            //Arrange
+            int expected = 2;
+            CustomList<int> list = new CustomList();
+            //Act
+            list.AddInt(2);
+            //Assert
+            Assert.AreEqual(expected, list[0]);
+        }
+        [TestMethod]
+        public void RemoveInt()
+        {
+            //Arrange
+            int expected = 0;
+            CustomList<int> list = new CustomList() {2};
+            //Act
+            list.RemoveInt(list[0]);
+            //Assert
+            Assert.AreEqual(expected, list.Count);
+        }
+        [TestMethod]
+        public void ClearList()
+        {
+            //Arrange
+            int expected = 0;
+            CustomList<int> list = new CustomList() { 1, 2, 3 };
+            //Act
+            list.ClearList();
+            //Assert
+            Assert.AreEqual(expected, list.Count);
         }
     }
 }
