f36edcacde92ea09bd77078c7ca27fa579794db6
jaredburks <jarabu93@gmail.com>
2017-09-20 11:55:41 -0500

    Start of project.

---
+++ CustomListClass/CustomListClass/CustomListTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListClass
+{
+    [TestClass]
+    public class CustomListTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
