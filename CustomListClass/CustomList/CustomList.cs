80c1bd21344fdcc333b02d47e62bb77b4044ab1a
jaredburks <jarabu93@gmail.com>
2017-09-21 11:34:17 -0500

    Added CustomList class and started layout of methods.

---
+++ CustomListClass/CustomList/CustomList.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class CustomList
+    {
+        public CustomList()
+        {
+
+        }
+
+        public void Add()
+        {
+
+        }
+        public void Remove()
+        {
+
+        }
+        public void Sort()
+        {
+
+        }
+        public void Count()
+        {
+
+        }
+        public void Zip()
+        {
+
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+        public static PlusOperator operator+(CustomList<T> a , CustomList<T> b)
+        {
+
+        }
+        public static MinusOperator operator -(CustomList<T> a, CustomList<T> b)
+        {
+
+        }
+    }
+}
